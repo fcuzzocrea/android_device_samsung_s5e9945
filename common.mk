87755a264a92f1b09f2dddcbc88067d061a93eb5
Michael Bestas <mkbestas@lineageos.org>
2025-07-05 17:12:11 +0300

    s5e9945: Include additional wlan soong namespace
    Change-Id: I1614960d2ca7a15f1646185986e2eae055e4b51d

--- common.mk
+++ common.mk
@@ -303,6 +303,7 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/google/interfaces \
     hardware/google/pixel \
     hardware/qcom-caf/wlan \
+    hardware/qcom-caf/wlan/qcwcn \
     hardware/samsung \
     hardware/samsung_slsi-linaro/exynos/cpboot_v3 \
     $(COMMON_PATH)
