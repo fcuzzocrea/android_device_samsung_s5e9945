d26aa558ea66d4f4ae86d29186ff3fcab088cef5
Francescodario Cuzzocrea <bosconovic@gmail.com>
2025-12-12 11:50:56 +0100

    update

--- common.mk
+++ common.mk
@@ -93,7 +93,7 @@ PRODUCT_PACKAGES += \
 # Graphics
 PRODUCT_PACKAGES += \
     android.hardware.composer.hwc3-service.slsi \
-    android.hardware.graphics.allocator-service-sgr \
+    android.hardware.graphics.allocator-aidl-service-sgr \
     android.hardware.graphics.mapper@4.0-impl-sgr \
     libdrm_sgpu \
     libexynosgraphicbuffer_public \
