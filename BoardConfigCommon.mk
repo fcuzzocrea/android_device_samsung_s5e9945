2dd268dd1689d8ba7a15de5ac1ad36adbf612ab4
Francescodario Cuzzocrea <bosconovic@gmail.com>
2026-05-31 21:56:35 +0200

    thats a +

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -152,7 +152,7 @@ $(call soong_config_set,samsungUsbGadgetVars,gadget_name,17900000.dwc3)
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
-BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS := --flags 3
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
 BOARD_AVB_RECOVERY_ALGORITHM := NONE
 BOARD_AVB_RECOVERY_KEY_PATH := external/avb/test/data/testkey_rsa4096.pem
 BOARD_AVB_RECOVERY_ROLLBACK_INDEX := 0
